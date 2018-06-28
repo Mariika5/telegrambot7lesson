655d80b92e1c89bf69ab9f09c2160b2e0c875787
mariika5 <mariika5@mail.ru>
2018-06-28 13:49:20 +0300

    Chat bot

---
+++ bot.rb
@@ -0,0 +1,31 @@
+require 'telegram/bot'
+
+TOKEN = '573052450:AAHdRpHBqXKh_qCewbQ7CfVmW71sxi3ADIA'
+
+ANSWERS = [
+	'У меня все хорошо',
+	'Да',
+	'Нет',
+	'Может быть',
+	'Попробуй',
+	'Да, конечно'
+]
+
+def answer(message)
+	if message.text == '123'
+		"Приветик, #{message.from.first_name}"	
+	else
+		ANSWERS.sample	
+	end
+end
+
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+	bot.listen do |message|
+		puts message
+		bot.api.send_message(
+          chat_id: message.chat.id,
+          text: answer(message)
+			)
+	end
+end
