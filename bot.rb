79e19fc29c7ee0acaaa9bf32a2b87c0d07074448
mariika5 <mariika5@mail.ru>
2018-07-01 14:47:36 +0300

    hash fot wdays

--- bot.rb
+++ bot.rb
@@ -14,6 +14,16 @@ ANSWERS = [
 	'Позвони другу срочно'
 ]
 
+DAYS = {
+	0 => "Сегодня воскресенье",
+	1 => "Вчера была среда, сегодня понедельник"
+	2 => "Сегодня вторник"
+	3 => "Сегодня среда"
+	4 => "Четверг"
+	5 => "Пятница, долой работу"
+	6 => "Сегодня суббота! Все на шашлыки!"
+}
+
 def answer(message)
 	if message.text == '/start'
 		"Привет, #{message.from.first_name}! Я умею отвечать на следующие вопросы: На каком языке ты написан?, Кто твой автор?, Сколько времени?, Какой сегодня день недели?, Сколько времени будет через час? И на любые другие ваши вопросы я тоже умею отвечать."
@@ -21,32 +31,18 @@ def answer(message)
 		"Я написан на Ruby"	
 	elsif message.text == 'Кто твой автор?'
 		"Меня сговнокодила Мария и я доволен собой"	
-    elsif message.text == 'Сколько времени?'
+	elsif message.text == 'Сколько времени?'
 		date = Time.now
-        date.strftime "%H:%M"
+		date.strftime "%H:%M"
 	elsif message.text == 'Какой сегодня день недели?'
 		date = Time.now
-			i = date.wday  
-			if i == 0
-			   "Сегодня воскресенье"
-			elsif i == 1
-			   "Вчера была среда сегодня понедельник"
-			elsif i == 2
-			   "Сегодня вторник"
-			elsif i == 3
-			   "Сегодня реда"
-			elsif i == 4
-			   "Четверг"
-			elsif i == 5
-			   "Пятница, долой работу"
-			elsif i == 6
-			   "Сегодня суббота! Все на шашлыки!"
-			end
+		i = date.wday 
+		DAYS[i]
 	elsif message.text == 'Сколько времени будет через час?'
 		date = Time.now
-        newtime = date + (60 * 60)
-        "Через час будет #{newtime.strftime '%H:%M'} по UTC"
-    else
+		newtime = date + (60 * 60)
+		"Через час будет #{newtime.strftime '%H:%M'} по UTC"
+	else
 		ANSWERS.sample	
 	end
 end
@@ -56,16 +52,9 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
 	bot.listen do |message|
 		puts message
 		bot.api.send_message(
-          chat_id: message.chat.id,
-          text: answer(message)
+			chat_id: message.chat.id,
+			text: answer(message)
 			)
 	end
 end
 
-
-
-# "На каком языке ты написан?"
-# "Кто твой автор?"
-# "Сколько времени?"
-# "Какой сегодня день недели?"
-# "Сколько времени будет через час?"
